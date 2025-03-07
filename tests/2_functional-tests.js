4205e283c9957fdc66e813fb6d233cac06fae3ec
Jaarabytes <xavierandole78@gmail.com>
2025-03-07 13:33:01 +0300

    ifix: tests fully work now
    tests using mocha were set to use suite and test and NOT describe and it

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,120 +1,125 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
-const expect = chai.expect; // Missing expect definition
 const server = require('../server');
-const { suite } = require('mocha');
-
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    describe("GET request to /api/stock-prices", () => {
-        it('should return a 400 error if no stock parameter is provided', function(done) {
-            chai
-              .request(server)
-              .get('/api/stock-prices')
-              .end(function(err, res) {
-                expect(res).to.have.status(400);
-                expect(res.body).to.be.an('object');
-                expect(res.body).to.have.property('error', 'Stock parameter is required');
-                done();
-              });
-        });        
-    });
+  test('should return a 400 error if no stock parameter is provided', function(done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices')
+      .end(function(err, res) {
+        assert.equal(res.status, 400);
+        assert.isObject(res.body);
+        assert.property(res.body, 'error');
+        assert.equal(res.body.error, 'Stock parameter is required');
+        done();
+      });
+  });
 
-    describe("GET request to /api/stock-prices?stock=GOOG&like=true", () => {
-        it("Viewing one stock and liking it", (done) => {
-            chai.request(server)
-            .get('/api/stock-prices?stock=GOOG&like=true')
-            .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res.body).to.have.property('stockData');
-                expect(res.body.stockData).to.have.property('stock', 'GOOG');
-                expect(res.body.stockData).to.have.property('price');
-                expect(res.body.stockData.price).to.be.a('number'); // Changed from 'GOOG' to 'number'
-                expect(res.body.stockData).to.have.property('likes');
-                expect(res.body.stockData.likes).to.be.a('number'); // Changed from equal(1) to be.a('number')
-                done();
-            });
-        });
-    });
+  test("Viewing one stock and liking it", function(done) {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        assert.property(res.body.stockData, 'price');
+        assert.isNumber(res.body.stockData.price);
+        assert.property(res.body.stockData, 'likes');
+        assert.isNumber(res.body.stockData.likes);
+        done();
+      });
+  });
 
-    describe("GET request to /api/stock-prices", () => {
-        it("Viewing the same stock and liking it again", (done) => {
-            chai.request(server)
-            .get('/api/stock-prices?stock=GOOG&like=true')
-            .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res.body).to.have.property('stockData');
-                expect(res.body.stockData).to.have.property('stock', 'GOOG');
-                expect(res.body.stockData).to.have.property('price');
-                expect(res.body.stockData.price).to.be.a('number'); // Changed from integer to 'number'
-                expect(res.body.stockData).to.have.property('likes');
-                expect(res.body.stockData.likes).to.be.a('number'); // Changed from rel_likes to likes
-                done();
-            });
-        });
-    });
+  test("Viewing the same stock and liking it again", function(done) {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        assert.property(res.body.stockData, 'price');
+        assert.isNumber(res.body.stockData.price);
+        assert.property(res.body.stockData, 'likes');
+        assert.isNumber(res.body.stockData.likes);
+        done();
+      });
+  });
 
-    describe("GET request to /api/stock-prices", () => {
-        it("Viewing two stocks", (done) => {
-            chai.request(server)
-                .get('/api/stock-prices?stock=GOOG&stock=MSFT')
-                .end((err, res) => {
-                    expect(err).to.be.null;
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('stockData');
-                    expect(res.body.stockData).to.be.an('array');
-                    expect(res.body.stockData).to.have.lengthOf(2);
-    
-                    const googStock = res.body.stockData.find(stock => stock.stock === 'GOOG');
-                    const msftStock = res.body.stockData.find(stock => stock.stock === 'MSFT');
-    
-                    expect(googStock).to.be.an('object');
-                    expect(msftStock).to.be.an('object');
-    
-                    expect(googStock).to.have.property('stock', 'GOOG');
-                    expect(msftStock).to.have.property('stock', 'MSFT');
-    
-                    expect(googStock).to.have.property('price').that.is.a('number');
-                    expect(msftStock).to.have.property('price').that.is.a('number');
-    
-                    expect(googStock).to.have.property('rel_likes').that.is.a('number');
-                    expect(msftStock).to.have.property('rel_likes').that.is.a('number');
-    
-                    done();
-                });
-        });
-    });
+  test("Viewing two stocks", function(done) {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT')
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
 
-    describe("GET request to /api/stock-prices", () => {
-        it("Viewing two stocks and liking them both", (done) => {
-            chai.request(server)
-            .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
-            .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                expect(res.body).to.have.property('stockData');
-                expect(res.body.stockData).to.be.an('array');
-                expect(res.body.stockData).to.have.lengthOf(2);
-                
-                const googStock = res.body.stockData.find(stock => stock.stock === 'GOOG');
-                const msftStock = res.body.stockData.find(stock => stock.stock === 'MSFT');
-                
-                expect(googStock).to.have.property('stock', 'GOOG');
-                expect(msftStock).to.have.property('stock', 'MSFT');
-                
-                expect(googStock).to.have.property('price').that.is.a('number');
-                expect(msftStock).to.have.property('price').that.is.a('number');
-                
-                expect(googStock).to.have.property('rel_likes').that.is.a('number');
-                expect(msftStock).to.have.property('rel_likes').that.is.a('number');
-                
-                done();
-            });
-        });
-    });
+        const googStock = res.body.stockData.find(stock => stock.stock === 'GOOG');
+        const msftStock = res.body.stockData.find(stock => stock.stock === 'MSFT');
+
+        assert.isObject(googStock);
+        assert.isObject(msftStock);
+
+        assert.property(googStock, 'stock');
+        assert.equal(googStock.stock, 'GOOG');
+        assert.property(msftStock, 'stock');
+        assert.equal(msftStock.stock, 'MSFT');
+
+        assert.property(googStock, 'price');
+        assert.isNumber(googStock.price);
+        assert.property(msftStock, 'price');
+        assert.isNumber(msftStock.price);
+
+        assert.property(googStock, 'rel_likes');
+        assert.isNumber(googStock.rel_likes);
+        assert.property(msftStock, 'rel_likes');
+        assert.isNumber(msftStock.rel_likes);
+
+        done();
+      });
+  });
+
+  test("Viewing two stocks and liking them both", function(done) {
+    chai.request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        
+        const googStock = res.body.stockData.find(stock => stock.stock === 'GOOG');
+        const msftStock = res.body.stockData.find(stock => stock.stock === 'MSFT');
+        
+        assert.isObject(googStock);
+        assert.isObject(msftStock);
+        
+        assert.property(googStock, 'stock');
+        assert.equal(googStock.stock, 'GOOG');
+        assert.property(msftStock, 'stock');
+        assert.equal(msftStock.stock, 'MSFT');
+        
+        assert.property(googStock, 'price');
+        assert.isNumber(googStock.price);
+        assert.property(msftStock, 'price');
+        assert.isNumber(msftStock.price);
+        
+        assert.property(googStock, 'rel_likes');
+        assert.isNumber(googStock.rel_likes);
+        assert.property(msftStock, 'rel_likes');
+        assert.isNumber(msftStock.rel_likes);
+        
+        done();
+      });
+  });
 });
